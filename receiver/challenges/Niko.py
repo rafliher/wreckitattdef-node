fa8b2518858253039dd9e2372b012e40082253ee
Rayhan Hanaputra <hanaputrarayhan@gmail.com>
2024-08-15 13:41:01 +0700

    added niko checker

--- receiver/challenges/Niko.py
+++ receiver/challenges/Niko.py
@@ -25,9 +25,20 @@ class Niko(Challenge):
             return False
 
     def check(self):
+        error_messages = [
+            "あなたはどんなオタクですか",
+            "冗談じゃないよ！",
+            "ｔｃｈ なんだよ こいつ",
+            "どうしてそんなことが可能でしょうか…不可能です",
+            "本当のあなたは何ですか？",
+            "うわー、ごめんなさい",
+            "御心のままに、主よ",
+            "もういいよ、やめて！",
+            "時間です",
+            "悪くないよ。"
+        ]
         try:
-            
-            # Step 3: Check if the flag still exists and matches the one in the container
+            # Step 1: Check if the flag still exists and matches the one in the container
             with open(self.flag_location, 'r') as f:
                 host_flag = f.read().strip()
 
@@ -39,7 +50,32 @@ class Niko(Challenge):
 
             assert host_flag == container_flag, 'Flag mismatch between host and container'
             
-            self.logger.info('Check passed for niko')
+            self.logger.info('Flag check passed for niko')
+            
+            # Step 2: Check if the webpage can be accessed
+            url = f'http://localhost:{self.port}/'
+            response = requests.get(url)
+            assert response.status_code == 200, 'Webpage is not accessible'
+            self.logger.info('Webpage accessibility check passed for niko')
+
+            # Step 3: Check if the output of the specific URL equals the expected string
+            urlFlag = f'http://localhost:{self.port}/api/getFlag?flag=1'
+            expected_output = "(⋟﹏⋞) 私をバカにしようとしているのか (´ ͡༎ຶ ͜ʖ ͡༎ຶ `)︵‿︵"
+            response = requests.get(urlFlag)
+            assert response.text.strip() == expected_output, 'Webpage output does not match expected output'
+            self.logger.info('Webpage output check passed for niko')
+            
+            # Step 4: Check if the chat endpoint is working
+            try:
+                urlChat = f'http://localhost:{self.port}/api/chat'
+                data = 'test'
+                response = requests.post(urlChat, data=data)
+                assert response.text.strip() in error_messages, 'Webpage is not accessible'
+                self.logger.info('Webpage delay check passed for niko')
+            except requests.exceptions.Timeout:
+                self.logger.error('Webpage request timed out')
+                return False
+
             return True
 
         except Exception as e:
