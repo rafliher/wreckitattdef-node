44c2f22131f42b551341938dbbccadeb36d506d3
fixxall <faturrohmansugiyarto@gmail.com>
2024-08-15 22:02:24 +0700

    fix and add checker for blinkpdf

--- receiver/challenges/Blinkpdf.py
+++ receiver/challenges/Blinkpdf.py
@@ -1,4 +1,5 @@
 from .Challenge import Challenge
+from modules.blinkpdf import *
 
 import io
 import requests
@@ -36,36 +37,57 @@ class Poke(Challenge):
             private_key = container_env.split("\n")[1].split('PRIVATE_KEY')[1]
 
             sess = requests.Session()
+
             # Checking C1: Login as user
             url = f'http://localhost:{self.port}/login'
             data = {'username': "user", "password": "user"}
             r = sess.post(url, data=data, timeout=5)
             assert 'Welcome to PDF Signature App'.lower() in r.text.lower(), 'Cannot login as user'
 
-            # Checking C2: Sign as user
+            pdfpath = 'files/blinkpdf_hellodocs.pdf'
+            pdfbytes = open(pdfpath, 'rb').read()
+
+            # Checking C2: Sign pdf as user
             sign_url = f'http://localhost:{self.port}/sign'
             r = sess.post(sign_url, timeout=5)
-            assert r.status_code == 200 and 'image/png' in r.headers['Content-Type'], 'Pokémon image not available or incorrect content type'
-            
-            # Checking C3: Verify as user
-            verify_url = f'http://localhost:{self.port}/verify'
-            r = sess.post(verify_url, timeout=5)
-            
-            
-            with open(self.flag_location, 'r') as f:
-                host_flag = f.read().strip()
+            sendata = ('main.pdf', pdfbytes, 'application/pdf')
+            filedata = {'file': sendata}
+            r = sess.post(sign_url, files=filedata, timeout=5)
+            signed_pdf = r.content
+            assert r.status_code == 200 and 'application/pdf' in r.headers['Content-Type'], 'Signed PDF not available or incorrect content type'
+            signed_pdf_stream = io.BytesIO(signed_pdf)
+            assert verify_signature(signed_pdf_stream, private_key), 'Algorithm for signature process is changed'
 
-            container_flag = subprocess.run(
-                ["docker", "exec", "poke_container", "cat", "/flag.txt"],
-                capture_output=True,
-                text=True
-            ).stdout.strip()
+            # Checking C3: Verify valid pdf as user
+            pdf_bytes_stream = io.BytesIO(pdfbytes)
+            signed_pdf_stream = sign_pdf(pdf_bytes_stream, private_key)
+            verify_url = f'http://localhost:{self.port}/verify'
+            sendata = ('main_signed.pdf', signed_pdf_stream, 'application/pdf')
+            filedata = {'file': sendata}
+            r = sess.post(verify_url, files=filedata, timeout=5)
+            assert 'The signature is valid' in r.text, 'Verify function not working or algoritm verify process is changed'
             
+            # Checking C3: Verify invalid pdf as user
+            verify_url = f'http://localhost:{self.port}/verify'
+            sendata = ('main_signed.pdf', pdfbytes, 'application/pdf')
+            filedata = {'file': sendata}
+            r = sess.post(verify_url, files=filedata, timeout=5)
+            assert 'The signature is invalid' in r.text, 'Verify function not working or algoritm verify process is changed for invalid signature'
+            
+            # Checking C4: Login as admin
+            url = f'http://localhost:{self.port}/login'
+            data = {'username': "admin", "password": f'{private_key}'}
+            r = sess.post(url, data=data, timeout=5)
+            assert 'Welcome to PDF Signature App'.lower() in r.text.lower(), 'Cannot login as user'
+
+            pdfpath = 'files/blinkpdf_hellodocs.pdf'
+            pdfbytes = open(pdfpath, 'rb').read()
+
             assert host_flag == container_flag, 'Flag mismatch between host and container'
 
-            self.logger.info('Check passed for poke')
+            self.logger.info('Check passed for blinkpdf')
             return True
 
         except Exception as e:
-            self.logger.error(f'Could not check poke: {e}')
+            self.logger.error(f'Could not check blinkpdf: {e}')
             return False
---
+++ receiver/modules/blinkpdf/__init__.py
@@ -0,0 +1,4 @@
+from blinkpdf.signature import *
+
+# Our checker is using decdsa.py and signature.py for validator. Avoid to change this 2 module, 
+# but you can change it if the verify and sign function work as properly.  
