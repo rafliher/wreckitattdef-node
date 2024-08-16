bbc270d2a5d09f27ba1f3231ac59d3750ccbe340
fixxall <faturrohmansugiyarto@gmail.com>
2024-08-16 15:17:08 +0700

    force docker

--- services/blinkpdf/main.sh
+++ services/blinkpdf/main.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 /usr/sbin/sshd -D
 
-# Load environment variables from .env file
-# set -a
-# . /opt/.env
-# set +a
-
 # exec python -u /opt/init.py
 
-# sleep 2
+sleep 2
 
-# exec python -u /opt/app.py 
+exec python -u /opt/app.py 
